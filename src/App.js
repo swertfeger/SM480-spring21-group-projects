f2c626cf0a3dbc2b4ebb35af0a30e25420e2ca45
Will Johnson <hi@designbywillj.com>
2021-02-10 11:57:35 -0600

    Fixed some styles for button and changed props

--- src/App.js
+++ src/App.js
@@ -9,21 +9,6 @@ import Icon from './js/components/Icon';
 function App() {
   return (
     <div className='App'>
-      <Button buttontype="primary-button" full>
-        <p>Continue</p>
-      </Button>
-      <Button buttontype="secondary-button" icon="google" full>
-        <p>Continue with Google</p>
-      </Button>
-      <Button buttontype="secondary-button" icon="twitter" full>
-        <p>Continue with Twitter</p>
-      </Button>
-      <Button buttontype="secondary-button" icon="facebook" full>
-        <p>Continue with Facebook</p>
-      </Button>
-      <Button buttontype="secondary-button" icon="apple" full>
-        <p>Continue with Apple</p>
-      </Button>
       <LoginForm />
     </div>
   );
--- src/js/components/LoginForm.jsx
+++ src/js/components/LoginForm.jsx
@@ -1,47 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import OAuth2Login from 'react-simple-oauth2-login';
+import React from "react";
+import ReactDOM from "react-dom";
+import OAuth2Login from "react-simple-oauth2-login";
 import classnames from "classnames";
 import PropTypes from "prop-types";
-import Input from './Input';
+import Input from "./Input";
+import Button from "./Button";
 
 function LoginForm(props) {
+  const onSuccess = (response) => console.log("SUCCESS: ", response);
+  const onFailure = (response) => console.error("ERROR: ", response);
 
-    const onSuccess = response => console.log("SUCCESS: ", response);
-    const onFailure = response => console.error("ERROR: ", response);
-    
-    return (
-        <div className={"root"}>
+  return (
+    <div className={"root"}>
+      <Button buttontype='secondary' full>
+        Continue
+      </Button>
+      <Button buttontype='secondary' icon='google' full>
+        Continue with Google
+      </Button>
+      <Button buttontype='secondary' icon='twitter' full>
+        Continue with Twitter
+      </Button>
+      <Button buttontype='secondary' icon='facebook' full>
+        Continue with Facebook
+      </Button>
+      <Button buttontype='secondary' icon='apple' full>
+        Continue with Apple
+      </Button>
 
-            <div>
-                <OAuth2Login
-                    authorizationUrl="https://accounts.spotify.com/authorize"
-                    responseType="token"
-                    clientId="9822046hvr4lnhi7g07grihpefahy5jb"
-                    redirectUri="http://localhost:3000/oauth-callback"
-                    onSuccess={onSuccess}
-                    onFailure={onFailure}
-                    buttonText={props.loginBtnText}
-                    className={"oAuthLogin"}
-                />
-            </div>
+      <div>
+        <OAuth2Login
+          authorizationUrl='https://accounts.spotify.com/authorize'
+          responseType='token'
+          clientId='9822046hvr4lnhi7g07grihpefahy5jb'
+          redirectUri='http://localhost:3000/oauth-callback'
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          buttonText={props.loginBtnText}
+          className={"oAuthLogin"}
+        />
+      </div>
 
-            <div 
-                className={classnames("fb-login-button", "fbButton")}
-                data-width=""
-                data-size="large" 
-                data-button-type="login_with"
-                data-layout="rounded"
-                data-auto-logout-link="false"
-                data-use-continue-as="false"
-            />
+      <div
+        className={classnames("fb-login-button", "fbButton")}
+        data-width=''
+        data-size='large'
+        data-button-type='login_with'
+        data-layout='rounded'
+        data-auto-logout-link='false'
+        data-use-continue-as='false'
+      />
 
-        </div>
-    )
+      <Button buttontype='primary'>Yes</Button>
+    </div>
+  );
 }
 
 LoginForm.propTypes = {
-    loginBtnText: PropTypes.string.isRequired,
-}
+  loginBtnText: PropTypes.string.isRequired,
+};
 
 export default LoginForm;
