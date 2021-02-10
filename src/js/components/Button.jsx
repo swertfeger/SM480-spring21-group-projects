7ba5d8b3fd2a73355ff303fa99a04f70832afb4f
rw-coding <rwalker@nossi.edu>
2021-02-10 11:03:19 -0600

    Icons added to buttons

--- src/js/components/Button.jsx
+++ src/js/components/Button.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import ReactDOM from 'react-dom';
 import styles from '../../scss/global.scss';
 import classnames from 'classnames';
+import Icon from './Icon';
 
 function Button(props) {
 
@@ -12,6 +13,7 @@ function Button(props) {
             {["half"]: props.half},
             {["full"]: props.full}
         )}>
+            {props.icon && <Icon type={props.icon} />}
             {props.children}
         </button>
     )
@@ -19,7 +21,8 @@ function Button(props) {
 }
 
 Button.propTypes = {
-    buttontype: PropTypes.oneOf(["primary-button", "secondary-button"])
+    buttontype: PropTypes.oneOf(["primary-button", "secondary-button"]),
+    icon: PropTypes.string
 }
 
 export default Button;
