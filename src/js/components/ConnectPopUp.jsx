3d64fc87e332570cea54bf1cf7d6e0c69513550e
Aly <mckalyn18@gmail.com>
2021-02-10 11:24:32 -0600

    second-commit
    added props serviceName

--- src/js/components/ConnectPopUp.jsx
+++ src/js/components/ConnectPopUp.jsx
@@ -8,7 +8,7 @@ function ConnectPopUp(props) {
     return (
         <div className="pop-up">
             <h1 className="pop-up-header"> Connect account? {props.name}</h1>
-            <p className="pop-up-para">You have previously logged in with your email. Would you like to connect your Google account to your App account?</p>
+            <p className="pop-up-para">You have previously logged in with your email. Would you like to connect your Google account to your App account?{props.serviceName}</p>
             <div className="pop-up-button">
                 <button className="no-button">No, thanks</button>
                 <button className="connect-button">Connect</button>
@@ -19,6 +19,7 @@ function ConnectPopUp(props) {
 
 ConnectPopUp.propTypes = {
     name: PropTypes.string.isRequired,
+    serviceName: PropTypes.string,
 }
 
 export default ConnectPopUp;
