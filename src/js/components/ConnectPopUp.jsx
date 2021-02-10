dbd5960f7f25619dc42b8f3f375fe4c641e9d07c
Aly <mckalyn18@gmail.com>
2021-02-10 11:27:19 -0600

    third-commit
    moved serviceName placement

--- src/js/components/ConnectPopUp.jsx
+++ src/js/components/ConnectPopUp.jsx
@@ -8,7 +8,7 @@ function ConnectPopUp(props) {
     return (
         <div className="pop-up">
             <h1 className="pop-up-header"> Connect account? {props.name}</h1>
-            <p className="pop-up-para">You have previously logged in with your email. Would you like to connect your Google account to your App account?{props.serviceName}</p>
+            <p className="pop-up-para">You have previously logged in with your email. Would you like to connect your {props.serviceName} account to your App account?</p>
             <div className="pop-up-button">
                 <button className="no-button">No, thanks</button>
                 <button className="connect-button">Connect</button>
