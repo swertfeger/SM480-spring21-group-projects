f512c4e03d577831b1d32c963de3d25672c17d14
Will Johnson <hi@designbywillj.com>
2021-02-10 03:37:31 -0600

    Building out basics of input component and organizing some styles

--- src/js/components/Input.jsx
+++ src/js/components/Input.jsx
@@ -1,17 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import OAuth2Login from "react-simple-oauth2-login";
+import React, { useState } from "react";
 import "./Input.scss";
-import classnames from "classnames";
-
-function Input() {
+import PropTypes from "prop-types";
+function Input(props) {
   return (
     <div className='input'>
-      <label htmlFor='' className='input__label' />
-      <input type='text' className='input__field' />
-      <div className='input__error'></div>
+      <label htmlFor='' className='input__label'>
+        {props.label}
+      </label>
+      <input
+        type='text'
+        className='input__field'
+        placeholder={props.placeholder}
+        value={props.value}
+      />
+      <div className='input__error'>
+        <div className='input__error-icon'></div>
+        <div className='input__error-message'>{props.error}</div>
+      </div>
     </div>
   );
 }
+
+Input.propTypes = {
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  error: PropTypes.string,
+};
 
 export default Input;
