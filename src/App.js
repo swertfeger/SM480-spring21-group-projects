9d1645b6f295ba8e817b912ca03c6487d971dcd7
Aly <mckalyn18@gmail.com>
2021-02-10 11:03:20 -0600

    first-commit-alyssa
    adding pop up connection option. mainly css

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {BrowserRouter} from "react-router-dom";
 import {hot} from "react-hot-loader";
 import "./App.scss";
 import LoginForm from "./js/components/LoginForm";
+import ConnectPopUp from "./js/components/ConnectPopUp";
 
 function App() {
   return (
     <div className='App'>
       <LoginForm />
+      <ConnectPopUp/>
     </div>
   );
 }
---
+++ src/js/components/ConnectPopUp.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./ConnectPopUp.scss";
+
+class ConnectPopUp extends React.Component {
+    render() {
+      return <div className="pop-up">
+                    <h1 className="pop-up-header"> Connect account? {this.props.name}</h1>
+                    <p className="pop-up-para">You have previously logged in with your email. Would you like to connect your Google account to your App account?</p>
+                    <div className="pop-up-button">
+                        <button className="no-button">No, thanks</button>
+                        <button className="connect-button">Connect</button>
+                    </div>
+                </div>;
+    }
+  }
+
+export default ConnectPopUp;
