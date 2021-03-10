ece8f1f48b0ec259a0c954fa7fd8352273ff7188
Grant Swertfeger <grant@swordsweeper.com>
2021-03-10 08:50:25 -0600

    Example data

--- src/js/pages/TwitterSearch.jsx
+++ src/js/pages/TwitterSearch.jsx
@@ -14,10 +14,35 @@ function TwitterSearch(props) {
   const searchTwitter = (query) => {
     return TwitterAPI.searchTweets(query);
   };
+  // EXAMPLE CHART DATA:
+  // data: {
+  //   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  //       datasets: [{
+  //     label: '# of Votes',
+  //     data: [12, 19, 3, 5, 2, 3],
+  //     backgroundColor: [
+  //       'rgba(255, 99, 132, 0.2)',
+  //       'rgba(54, 162, 235, 0.2)',
+  //       'rgba(255, 206, 86, 0.2)',
+  //       'rgba(75, 192, 192, 0.2)',
+  //       'rgba(153, 102, 255, 0.2)',
+  //       'rgba(255, 159, 64, 0.2)'
+  //     ],
+  //     borderColor: [
+  //       'rgba(255, 99, 132, 1)',
+  //       'rgba(54, 162, 235, 1)',
+  //       'rgba(255, 206, 86, 1)',
+  //       'rgba(75, 192, 192, 1)',
+  //       'rgba(153, 102, 255, 1)',
+  //       'rgba(255, 159, 64, 1)'
+  //     ],
+  //     borderWidth: 1
+  //   }]
+  // },
 
   console.log(twitterData);
   const mostRetweeted = twitterData
-    ? orderBy(twitterData.data, ["public_metrics.retweet_count"], ["desc"])
+    ? orderBy(twitterData, ["public_metrics.retweet_count"], ["desc"])
     : [];
   const top10Retweets = map(mostRetweeted.slice(0, 10), (tweet) => ({
     text: tweet.text,
